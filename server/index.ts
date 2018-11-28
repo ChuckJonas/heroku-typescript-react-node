cbbdeca6586b07fcfc83c49660f5a59b8f4d95ec
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-11-27 22:33:55 -0700

    fixing some types stuff

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
+import * as express from 'express';
+import * as path from 'path';
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-app/dist')));
@@ -13,8 +14,8 @@ app.get('/api/items', function (req, res) {
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-app/dist', 'index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../react-app/dist', 'index.html'));
 });
 
 app.listen(PORT, function () {
